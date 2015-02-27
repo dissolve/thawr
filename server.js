36d67e674c7ffe53a5b5229e358ae51468256b8f
Ben Roberts <ben@directdigitalllc.com>
2015-02-27 11:03:07 -0500

    starting to work on multi-room support

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var config = {
-    channels: ["#indiechat"],
+    channels: ["#indiechat","#indiewebcamp"],
     server: "irc.freenode.net",
     botName: "thawr"
 };
 
+var me = config.botName;
+var channels = config.channels;
+
+
 // Get the lib
 var irc = require("irc");
+var names = {}; 
 
 // Create the bot name
 var bot = new irc.Client(config.server, config.botName, {
@@ -24,23 +29,53 @@ app.get('/', function(req, res){
 
 bot.addListener("message", function(from, to, text, message) {
     //console.log('message received from IRC');
-    io.emit('message', "<" + from+"> " + text);
+    if(to = me){
+        //if it is a direct message, treat it as a chat room with 
+        //  room name = the other user's name
+        io.emit('message', from, from, text, message);
+    } else {
+        io.emit('message', from, to, text, message);
+    }
 });
+
+bot.addListener("join", function(channel, who) {
+    io.emit('join', channel, who);
+});
+
+bot.addListener("names", function(channel, nicks){
+    names[channel] = nicks;
+    io.emit('names', channel, nicks);
+});
+
+bot.addListener("quit", function(who, reason, channels,  message) {
+    io.emit('quit', who, reason, channels, message);
+});
+
+bot.addListener("part", function(channel, who, reason, message) {
+    io.emit('part', channel, who, reason, message);
+});
+
 
 io.on('connection', function(socket){
     //console.log('user connected');
+    io.emit('channels', channels);
+    if(names){
+        for(var chan in names){
+            io.emit('names', chan,  names[chan]);
+        }
+    }
 
 
-    socket.on('chat message', function(msg){
-        console.log('message from user: ' + msg);
+    socket.on('send message', function(room, msg){
+        //console.log('message from user: ' + msg);
         nickchange = msg.match('^/nick ([^ ]*)')
         if(nickchange){
             bot.send('NICK' , nickchange[1]);
-            config.botName = nickchange[1];
+            me = nickchange[1];
         } else {
-            bot.say('#indiechat', msg);
+            bot.say(room, msg);
         }
-        io.emit('message', "<" + config.botName +"> " + msg);
+        io.emit('message', me, room, msg, {});
     });
 });
 
